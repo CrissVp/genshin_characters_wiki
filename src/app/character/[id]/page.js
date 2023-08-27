9cdf3b9e58c6da9a98fb9d124c1e4b5bae1f08d0
CrissVp <crissvph@gmail.com>
2023-08-27 00:26:23 -0600

    1

--- src/app/character/[id]/page.js
+++ src/app/character/[id]/page.js
@@ -14,10 +14,10 @@ export default async function Character({ params }) {
             <ElementalBackground bg={data.vision.toLowerCase()} />
             <main className={styles.main}>
                 <div className={styles.content}>
-                    <section>
+                    <section className={styles.info_section}>
                         <div className={styles.character_info}>
                             <div className={styles.character_name}>
-                                <div className={styles.character_icon}>
+                                <div className={`${styles.character_icon} ${data.rarity === 5 ? styles.five_stars_bg : styles.four_stars_bg}`}>
                                     <Image alt={'character_icon'} height={50} width={50} src={images["icon-big"]} />
                                 </div>
                                 <span>{data.name}</span>
@@ -39,17 +39,51 @@ export default async function Character({ params }) {
                             />
                         </div>
                     </section>
-                    <div className={styles.details_table}>
-                        <h1>{data.name}</h1>
-                        <h2>{data.title}</h2>
-                        <h4>{data.vision}</h4>
-                        <h4>{data.weapon}</h4>
-                        <h4>{data.nation}</h4>
-                        <h4>{data.affiliation}</h4>
-                        <h4>{data.rarity}</h4>
-                        <h4>{data.constellation}</h4>
-                        <h4>{data.birthday}</h4>
-                    </div>
+                    <section className={`${styles.details_table} ${styles[`bg_${data.vision.toLowerCase()}`]}`}>
+                        <div className={styles.table_header}>
+                            <h4>Attributes</h4>
+                        </div>
+                        <div className={styles.table_row}>
+                            <div className={styles.table_column}>
+                                <span>Name</span>
+                                <p>{data.name}</p>
+                            </div>
+                            <div className={styles.table_column}>
+                                <span>Birthday</span>
+                                <p>{data.birthday}</p>
+                            </div>
+                        </div>
+                        <div className={styles.table_row}>
+                            <div className={styles.table_column}>
+                                <span>Constellation</span>
+                                <p>{data.constellation}</p>
+                            </div>
+                            <div className={styles.table_column}>
+                                <span>Title</span>
+                                <p>{data.title}</p>
+                            </div>
+                        </div>
+                        <div className={styles.table_row}>
+                            <div className={styles.table_column}>
+                                <span>Vision</span>
+                                <p>{data.vision}</p>
+                            </div>
+                            <div className={styles.table_column}>
+                                <span>Affiliation</span>
+                                <p>{data.affiliation}</p>
+                            </div>
+                        </div>
+                        <div className={styles.table_row}>
+                            <div className={styles.table_column}>
+                                <span>Weapon</span>
+                                <p>{data.weapon}</p>
+                            </div>
+                            <div className={styles.table_column}>
+                                <span>Nation</span>
+                                <p>{data.nation}</p>
+                            </div>
+                        </div>
+                    </section>
                 </div>
             </main>
         </>
