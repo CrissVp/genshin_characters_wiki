524c2294afaa774ad10d1f550fb6cb4c2b79ceaa
CrissVp <crissvph@gmail.com>
2023-09-12 17:37:42 -0600

    1

--- src/components/CharacterGallery/index.js
+++ src/components/CharacterGallery/index.js
@@ -10,23 +10,22 @@ import Button from '../Button';
 import styles from './styles.module.scss';
 
 export default function CharacterGallery({ data, vision }) {
-    const PIC_CONTAINER_WIDTH = data.pic ? 800 : 1052;
-
     const buttonList = useRef(null);
     const galleryPics = useRef(null);
     const galleryContainer = useRef(null);
 
-    const [activePage, setActivePage] = useState(data.list[0].key);
-    const [galleryItemWidth, setGalleryItemWidth] = useState(undefined);
+    const [itemWidth, setItemWidth] = useState(undefined);
+    const [characterPics, setCharacterPics] = useState([]);
     const [scrollButtonsVisible, setScrollButtonsVisible] = useState(false);
+    const [activePage, setActivePage] = useState({ key: data.list[0].key, index: 0 });
 
-    const scrollToItem = ({ element, x }) => {
-        element.current.style = `transform: translate3d(${x}px, 0px, 0px);`
+    const scrollToItem = ({ element, index }) => {
+        element.current.style = `transform: translate3d(${-itemWidth * index}px, 0px, 0px);`
     };
 
     const scrollGallery = (index, key) => {
-        scrollToItem({ element: galleryPics, x: -galleryItemWidth * index });
-        setActivePage(key);
+        scrollToItem({ element: galleryPics, index });
+        setActivePage({ key, index });
     };
 
     const scrollButtonsPrev = () => {
@@ -40,19 +39,38 @@ export default function CharacterGallery({ data, vision }) {
     useEffect(() => {
         const resizeObserver = new ResizeObserver((entries) => {
             const { height, width } = entries[0].contentRect;
-            console.log({ height, width });
-            setGalleryItemWidth(width);
+            setItemWidth(width);
         });
 
-        if (galleryContainer?.current) {
-            resizeObserver.observe(galleryContainer.current);
-        }
-
-        return () => resizeObserver.unobserve(galleryContainer.current);
+        if (galleryContainer?.current) resizeObserver.observe(galleryContainer.current);
+        return () => { if (galleryContainer?.current) resizeObserver.unobserve(galleryContainer.current) };
     }, [galleryContainer?.current]);
 
     useEffect(() => {
-        if (buttonList?.current) setScrollButtonsVisible(buttonList.current.scrollWidth > PIC_CONTAINER_WIDTH)
+        if (window.innerWidth < 1200 && data.pic) {
+            return setCharacterPics([
+                { key: 'Card', img: data.pic, imgDesc: '' },
+                ...data.list
+            ]);
+        }
+
+        setCharacterPics([...data.list]);
+    }, [itemWidth]);
+
+    useEffect(() => {
+        if (characterPics.length > 0) {
+            if (activePage.key === 'Card' && characterPics[0].key !== 'Card') {
+                return scrollGallery(0, characterPics[0].key);
+            }
+
+            const activeItemIndex = characterPics.findIndex((item) => item.key === activePage.key);
+            scrollGallery(activeItemIndex, activePage.key);
+        }
+    }, [characterPics]);
+
+    useEffect(() => {
+        if (buttonList?.current && galleryContainer.current)
+            setScrollButtonsVisible(buttonList.current.scrollWidth > itemWidth);
     }, [buttonList?.current]);
 
     return (
@@ -67,8 +85,8 @@ export default function CharacterGallery({ data, vision }) {
                         )}
                         <div className={styles.list_container}>
                             <div ref={buttonList} className={styles.buttons_list}>
-                                {data.list.map((item, index) => (
-                                    <Button active={activePage === item.key} handleClick={() => scrollGallery(index, item.key)} text={item.key} key={index} />
+                                {characterPics.map((item, index) => (
+                                    <Button active={activePage.key === item.key} handleClick={() => scrollGallery(index, item.key)} text={item.key} key={index} />
                                 ))}
                             </div>
                         </div>
@@ -79,12 +97,12 @@ export default function CharacterGallery({ data, vision }) {
                         )}
                     </div>
                     <div ref={galleryPics} className={styles.character_pics}>
-                        {data.list.map((item) => (
-                            <div key={item.key} className={`${styles.gallery_item} ${data.pic ? '' : styles.no_pic}`}
-                                style={galleryItemWidth && { minWidth: galleryItemWidth }}>
-                                {item.key === activePage && (
+                        {characterPics.map((item) => (
+                            <div key={item.key} className={`${styles.gallery_item}`}
+                                style={itemWidth && { minWidth: itemWidth }}>
+                                {item.key === activePage.key && (
                                     <>
-                                        <div className={`${styles.gallery_pic} ${data.pic ? '' : styles.no_pic}`}>
+                                        <div className={`${styles.gallery_pic} ${item.key === 'Card' ? styles.card : ''}`}>
                                             <Image src={item.img} width={450} height={450} alt='Gallery_Pic' placeholder="blur" blurDataURL="/load_img.png" />
                                         </div>
                                         <div className={styles.gallery_desc}>
