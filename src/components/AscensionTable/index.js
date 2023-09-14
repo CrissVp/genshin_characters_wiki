edf27e49858f0369f25fa6ee76383c7d977df628
CrissVp <crissvph@gmail.com>
2023-09-14 17:32:17 -0600

    1

--- src/components/AscensionTable/index.js
+++ src/components/AscensionTable/index.js
@@ -1,6 +1,6 @@
 'use client'
 
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import Image from 'next/image';
 
 import TableComponent from '../TableComponent';
@@ -11,11 +11,22 @@ import styles from './styles.module.scss';
 export default function AscensionTable({ data, vision }) {
     const buttonsContainer = useRef(null);
     const [pageData, setPageData] = useState(data[1]);
+    const [containerWidth, setContainerWidth] = useState(buttonsContainer.current?.clientWidth);
+
+    useEffect(() => {
+        const resizeObserver = new ResizeObserver((entries) => {
+            const { height, width } = entries[0].contentRect;
+            setContainerWidth(width);
+        });
+
+        if (buttonsContainer.current) resizeObserver.observe(buttonsContainer.current);
+        return () => resizeObserver.unobserve(buttonsContainer.current);
+    }, []);
 
     return (
         <TableComponent title={'Ascension'} vision={vision}>
             <div ref={buttonsContainer} className={styles.button_list}>
-                <ButtonsList items={data} activeItem={pageData} itemOnClick={setPageData} containerWidth={buttonsContainer.current?.clientWidth} />
+                <ButtonsList items={data} activeItem={pageData} itemOnClick={setPageData} containerWidth={containerWidth} />
             </div>
             <div key={pageData.key} className={styles.data_table}>
                 <table className={styles.table}>
@@ -40,14 +51,16 @@ export default function AscensionTable({ data, vision }) {
                     <div className={styles.tr}>
                         <span className={styles.th}>Ascension Materials</span>
                     </div>
-                    <div className={`${styles.table_body} ${styles.materials}`}>
-                        {pageData.materials?.map((mat, index) => (
-                            <div key={index} className={styles.mat}>
-                                <Image src={mat.icon_url} height={20} width={20} alt='Material_Pic' />
-                                <span>{mat.name}</span>
-                                <span>x {mat.amount}</span>
-                            </div>
-                        ))}
+                    <div className={styles.table_body}>
+                        <div className={`${styles.materials}`}>
+                            {pageData.materials?.map((mat, index) => (
+                                <div key={index} className={styles.mat}>
+                                    <Image src={mat.icon_url} height={20} width={20} alt='Material_Pic' />
+                                    <span>{mat.name}</span>
+                                    <span>x {mat.amount}</span>
+                                </div>
+                            ))}
+                        </div>
                     </div>
                 </div>
             </div>
